e0ec096dcfc7935244f4ee590205459e9c2ed5a6
Dragon12346 <league@WTS13.attlocal.net>
2018-07-20 18:25:40 -0700

    Minion

--- src/Minion.java
+++ src/Minion.java
@@ -1,11 +1,33 @@
 
 public class Minion {
-
+	private String name; 
+	private int eyes; 
+	private String color; 
+	private String master;
 Minion (String name, int eyes, String color, String master) {
-	Minion q = new Minion("Stuart", 1, "yellow", "");
-	
-	
+	this.name = name;
+	this.eyes = eyes;
+	this.color = color;
+	this.master = master;
+}
+
+public String getName() {
+	return name;
+}
+public int getEyes() {
+	return eyes;
+}
+public String getColor() {
+	return color;
+}
+public String getMaster() {
+	return master;
+}
+void setMaster(String master){
+	this.master = master;
+}
 
 
+	
 }
-}
+
